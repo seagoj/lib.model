b49a13cca0af423f1d301bd8c70833c52640d719
Jeremy Seago <seagoj@gmail.com>
2012-01-18 16:55:12 -0600

    change file structure

---
+++ class.dbg.php
@@ -0,0 +1,29 @@
+<?php
+print "<div>Start class DBG ".rand()."</div>";
+/**
+ * Description of class
+ *
+ * @author jds
+ */
+class dbg
+{
+    function __construct()
+    {}
+    public function msg($message, $method='', $file='', $line='')
+    {
+        print "<div class='err'>";
+        $method=='' ? print '' : print "<span style='color:red;'>$method</span>: ";
+        print "<span style='color:black'>$message";
+        $file=='' ? print '' : print "in file $file";
+        $line=='' ? print '' : print "on line $line";
+        print "</span></div>";
+    }
+    public function vardump($var, $label='')
+    {
+        $dump = var_export($var, true);
+        print "<div class='err'>";
+        $label=='' ? print '' : print "<span style='color:red;'>$label</span>: ";
+        print "<span style='color:black;'>$dump</span></div>";
+    }
+}
+?>
