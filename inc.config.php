b315f0919804b63d3d85756b1f8611319d8cffdc
Jeremy Seago <seagoj@gmail.com>
2012-01-29 01:46:23 -0600

    flesh out class.model

---
+++ inc.config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("_DEBUG_", false);
+define("_DB_SERVER_", "mysql50-44.wc1.dfw1.stabletransit.com");
+define("_DB_USER_", "464119_nxtlvl");
+define("_DB_PASS_", "Onthesigs1845");
+
+?>
