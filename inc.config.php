c421906ba5e35df0257af38081c4cf336d7edb7d
Jeremy Seago <seagoj@gmail.com>
2012-02-06 17:49:40 -0600

    Remove unused files.

--- inc.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("_DEBUG_", false);
-define("_DB_SERVER_", "mysql50-44.wc1.dfw1.stabletransit.com");
-define("_DB_USER_", "464119_nxtlvl");
-define("_DB_PASS_", "Onthesigs1845");
-
-?>
